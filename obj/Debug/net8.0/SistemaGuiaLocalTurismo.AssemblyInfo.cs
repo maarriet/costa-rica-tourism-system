e61ab30345acee6bec8669eb26d3b262996c0490
Marco Arrieta <marcoantonioarrietacruz@gmail.com>
2025-12-09 08:52:41 -0600

    Switch to Nixpacks for .NET deployment

--- obj/Debug/net8.0/SistemaGuiaLocalTurismo.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaGuiaLocalTurismo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGuiaLocalTurismo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f003172cfee96d363b6773edbe39a97e05eaff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1934b6bb754cad308b8407327735bf20137d247d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGuiaLocalTurismo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGuiaLocalTurismo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
