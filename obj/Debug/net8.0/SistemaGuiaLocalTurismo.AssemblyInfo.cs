b67f11577354578ece3bd900fd1d5247fafd8f3d
Marco Arrieta <marcoantonioarrietacruz@gmail.com>
2025-12-11 13:06:15 -0600

    Fix Index reservation view 1.2

--- obj/Debug/net8.0/SistemaGuiaLocalTurismo.AssemblyInfo.cs
+++ obj/Debug/net8.0/SistemaGuiaLocalTurismo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaGuiaLocalTurismo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c381b1efbb9cee5a473ea61837b5e09c9549c811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ed2bdfdc94674f55eda490e6a55a6f7f03ab743")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaGuiaLocalTurismo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaGuiaLocalTurismo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
